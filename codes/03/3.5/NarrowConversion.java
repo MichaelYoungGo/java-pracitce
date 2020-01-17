564001115201dba4c5dc2559e5d494d44605cc10
MichealYoungGo <ustb_yhb@163.com>
2020-01-17 18:01:12 +0800

    3.5

---
+++ codes/03/3.5/NarrowConversion.java
@@ -0,0 +1,10 @@
+public class NarrowConversion {
+	public static void main(String[] args){
+		var iValue = 223;
+		byte bValue = (byte) iValue;
+	       	System.out.println(bValue);
+		var dValue = 3.98;
+		int tol = (int) dValue;
+		System.out.println(tol);	
+	}
+}
