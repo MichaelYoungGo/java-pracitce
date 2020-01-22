037218b0d4ecfc74bab43bf9dab284be8a699a04
MichealYoungGo <ustb_yhb@163.com>
2020-01-22 17:30:28 +0800

    happy new year

---
+++ codes/04/ForEachTest.java
@@ -0,0 +1,8 @@
+public class ForEachTest {
+	public static void main(String[] args){
+		String[] books = {"轻量级Java EE", "疯狂Java讲义", "疯狂Android讲义"};
+		for(String book : books){
+			System.out.println(book);
+		}
+	}
+}
