123db7f4d2cefcfa50cbffda06ba978afca17102
MichealYoungGo <ustb_yhb@163.com>
2020-01-25 16:25:34 +0800

    happy Chinese New Year

---
+++ codes/04/PrimitiveArreyTest.java
@@ -0,0 +1,9 @@
+public class PrimitiveArreyTest {
+	public static void main(String[] args){
+		int[] iArrey;
+		iArrey = new int[5];
+		for (var i = 0; i < iArrey.length; i++){
+			iArrey[i] = i + 10;
+		}
+	}
+}
