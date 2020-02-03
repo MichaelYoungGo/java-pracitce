86d51dd0dc0a949e0856680bae9f01ed27f3cd89
MichealYoungGo <ustb_yhb@163.com>
2020-02-03 21:01:01 +0800

    paint circle

--- codes/04/ChapterPractice/PrintCircle.java
+++ codes/04/ChapterPractice/PrintCircle.java
@@ -1,10 +1,10 @@
 import java.lang.Math;
 public class PrintCircle{
-	public static void main(Sting[] args){
+	public static void main(String[] args){
 		paint(10);
 	}
 	public static void paint(int r){
-		int y = r * 2；
+		int y = r * 2;
 		int x = 0;
 		int c = 0;
 		int z = 2;
@@ -12,7 +12,20 @@ public class PrintCircle{
 			x = getX(r, y);
 			System.out.print(getSpace(x) + "*");
 			c = (r - x ) * 2;
-
+			System.out.println(getSpace(c) + "*");
+			y -=z;
 		}
+	}
+	public static int getX(int r, int y){
+		int h = y -r;
+		double l = Math.sqrt((r*r)-(h*h));
+		return (int)Math.round(r-l);
+	}
+	public static String getSpace(int i){
+		String s = " ";
+	       	for (int j = 0; j < i; j++){
+			s += " ";
+		}
+		return s;
 	}
 }
