fb172028f9e7153bd93b9f254671e22b9c127d94
MichealYoungGo <ustb_yhb@163.com>
2020-03-15 17:29:00 +0800

    hashset

---
+++ codes/08/LinkedHashSetTest.java
@@ -0,0 +1,10 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetTest {
+	public static void main(String[] args){
+		var books = new LinkedHashSet();
+		books.add("疯狂Java讲义");
+		books.add("Test LinledHashSet");
+		System.out.println(books);
+	}
+}
