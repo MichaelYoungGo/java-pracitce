ac070900c14accba330b6fbd2dd481ea3fe7975f
MichealYoungGo <ustb_yhb@163.com>
2020-06-21 16:13:00 +0800

    多线程20200621

---
+++ codes/16/ThreadGroupTest.java
@@ -0,0 +1,36 @@
+import java.lang.Thread;
+
+class MyThread extends Thread
+{
+	public MyThread(String name)
+	{
+		super(name);
+	}
+	public MyThread(ThreadGroup group, String name)
+	{
+		super(group, name);
+	}
+	public void run()
+	{
+		for (var i = 0; i <20; i++)
+		{
+			System.out.println(getName() + "线程的i变量" + i);
+		}
+	}
+}
+public class ThreadGroupTest
+{
+	public static void main(String[] args)
+	{
+		ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
+		System.out.println("主线程组的名字：" + mainGroup.getName());
+		System.out.println("主线程组是否是后台线程组：" + mainGroup.isDaemon());
+		new MyThread("主线程组的线程").start();
+		var tg = new ThreadGroup("新线程组");
+		tg.setDaemon(true);
+		System.out.println("tg线程组是否是后台线程组：" + tg.isDaemon());
+		var tt = new MyThread(tg, "tg组的线程甲");
+		tt.start();
+		new MyThread(tg, "tg组的线程已").start();
+	}
+}
